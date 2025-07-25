6884b5231e8c2833c050139a9ae3f1fa5c0655e8
RajsekharG <rajsekhar961@gmail.com>
2025-07-25 11:46:12 +0530

    Resolved merge conflict in Loginpage.java

--- src/test/java/StepDefinitions/Loginpage.java
+++ src/test/java/StepDefinitions/Loginpage.java
@@ -37,17 +37,8 @@ public class Loginpage extends BaseClass {
 	@Then("the user navigates to the homepage.")
 	public void the_user_navigates_to_the_homepage() {
 		
-HEAD
 		String actualOutput = getPage().title();
-//	    Locator userNameLocator = getPage().locator("//p[@class='oxd-userdropdown-name']");
-//	    String observedoutput=userNameLocator.textContent();
-//	    System.out.println(observedoutput);
 	    Assert.assertEquals(actualOutput, "OrangeHRM","Title didnot matched");
-	    String actualoutput="john desuza";
-	    Locator userNameLocator = getPage().locator("//p[@class='oxd-userdropdown-name']");
-	    String observedoutput=userNameLocator.textContent();
-	    System.out.println(observedoutput);
-	    Assert.assertEquals(actualoutput, observedoutput);
 
 	}
 
