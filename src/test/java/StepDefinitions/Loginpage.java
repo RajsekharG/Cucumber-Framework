12a2d5382dad166fefd1c4fe358c275afc234134
RajsekharG <rajsekhar961@gmail.com>
2025-07-25 11:27:26 +0530

    corrections Done

--- src/test/java/StepDefinitions/Loginpage.java
+++ src/test/java/StepDefinitions/Loginpage.java
@@ -3,6 +3,8 @@ package StepDefinitions;
 import java.io.IOException;
 import org.junit.Assert;
 import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+
 import Playwright.Cucumberframework.BaseClass;
 import Playwright.Cucumberframework.PageObject;
 import io.cucumber.java.en.*;
@@ -35,11 +37,11 @@ public class Loginpage extends BaseClass {
 	@Then("the user navigates to the homepage.")
 	public void the_user_navigates_to_the_homepage() {
 		
-	    String actualoutput="manda user";
-	    Locator userNameLocator = getPage().locator("//p[@class='oxd-userdropdown-name']");
-	    String observedoutput=userNameLocator.textContent();
-	    System.out.println(observedoutput);
-	    Assert.assertEquals(actualoutput, observedoutput);
+		String actualOutput = getPage().title();
+//	    Locator userNameLocator = getPage().locator("//p[@class='oxd-userdropdown-name']");
+//	    String observedoutput=userNameLocator.textContent();
+//	    System.out.println(observedoutput);
+	    Assert.assertEquals(actualOutput, "OrangeHRM","Title didnot matched");
 	}
 
 }
